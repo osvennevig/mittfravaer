ec24ebefadfb2a380b2e083a648e93514e641ff7
Ole Svennevig <69307900+osvennevig@users.noreply.github.com>
2024-05-22 15:27:29 +0200

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,19 +13,36 @@ export default function Index() {
   return (
     <div>
       <div className="mx-4 mt-8 mb-8 text-center">
-        <h1 className="text-4xl font-bold mb-2 text-primary">
+        <h1 className="text-4xl font-extrabold mb-2 text-primary">
           Beregn fraværet ditt på sekunder.
         </h1>
         <p className="text-lg">Gjelder for høsten 2023 og våren 2024</p>
       </div>
       <Calculation />
-      <div className="m-4">
-        <p className="text-sm">
+      <div className="m-4 mx-auto max-w-xs">
+        <p className="text-sm text-center">
           Kalkulatoren tar for seg et helt år (høst + vår). <br />
           Derfor kan prosenten bli lavere enn den ofte vises i skolesystemene.
         </p>
       </div>
-      <Campaign />
+      {/* <Campaign /> */}
+      <footer className="m-4 border-t mt-8 mb-8">
+        <p className="text-sm text-center mt-4">
+          MittFravær av
+          <a
+            className="text-sm text-center block underline hover:text-primary"
+            href="https://svennevigflaaten.no"
+          >
+            Svennevig & Flaaten
+          </a>
+        </p>
+        <a
+          className="text-sm text-center block underline hover:text-primary"
+          href="https://instagram.com/mittfravaer"
+        >
+          Instagram
+        </a>
+      </footer>
     </div>
   );
 }
