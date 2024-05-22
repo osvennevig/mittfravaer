6be897fe67575cc96e6cd60f26bb2b602e4245ee
Ole Svennevig <69307900+osvennevig@users.noreply.github.com>
2024-05-22 13:25:01 +0200

    Update calculation.tsx

--- app/components/calculation.tsx
+++ app/components/calculation.tsx
@@ -1,5 +1,7 @@
 import { useState, useMemo } from "react";
+import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 import { Card, CardContent } from "~/components/ui/card";
+import { Button } from "./ui/button";
 
 export default function Calculation() {
   const [fag, setFag] = useState(1);
@@ -28,30 +30,65 @@ export default function Calculation() {
         <p className="text-2xl mx-4 mb-1 text-center">Din prosent er:</p>
         <p className="text-5xl font-extrabold mx-4 text-center">{calcResult}</p>
       </div>
-      <div className="pb-16 px-6 m-auto max-w-xl mt-8">
+      <div className="pb-16 px-6 m-auto max-w-fit mt-8">
         <Card>
-          <CardContent className="p-4">
+          <CardContent className="p-6">
             <div>
-              <label>
-                Antall udokumenterte fraværstimer:
-                <input
-                  type="number"
-                  value={udok}
-                  min="1"
-                  onChange={(e) => setUdok(e.target.value)}
-                />
-              </label>
+              <p className="mb-2">Antall udokumenterte fraværstimer:</p>
+
+              <div className="flex flex-row items-center gap-12 p-2 bg-secondary rounded-full">
+                <Button
+                  variant="default"
+                  size="icon"
+                  className="h-12 w-12 shrink-0 rounded-full"
+                  onClick={() => setUdok((prevUdok) => prevUdok - 1)}
+                  disabled={udok <= 1}
+                >
+                  <MinusIcon className="h-4 w-4" />
+                  <span className="sr-only">Trekk fra time</span>
+                </Button>
+                <span className="w-full text-center text-xl font-bold">
+                  {udok}
+                </span>
+                <Button
+                  variant="default"
+                  size="icon"
+                  className="h-12 w-12 shrink-0 rounded-full"
+                  onClick={() => setUdok((prevUdok) => prevUdok + 1)}
+                  disabled={udok >= 50}
+                >
+                  <PlusIcon className="h-4 w-4" />
+                  <span className="sr-only">Legg til time</span>
+                </Button>
+              </div>
             </div>
-            <div>
-              <label>
-                Antall timer i uken for gjeldende fag:
-                <input
-                  type="number"
-                  value={fag}
-                  min="1"
-                  onChange={(e) => setFag(e.target.value)}
-                />
-              </label>
+            <div className="mt-6">
+              <p className="mb-2">Antall timer i uken for gjeldende fag:</p>
+              <div className="flex flex-row items-center gap-12 p-2 bg-secondary rounded-full">
+                <Button
+                  variant="default"
+                  size="icon"
+                  className="h-12 w-12 shrink-0 rounded-full"
+                  onClick={() => setFag((prevFag) => prevFag - 1)}
+                  disabled={fag <= 1}
+                >
+                  <MinusIcon className="h-4 w-4" />
+                  <span className="sr-only">Trekk fra time</span>
+                </Button>
+                <span className="w-full text-center text-xl font-bold">
+                  {fag}
+                </span>
+                <Button
+                  variant="default"
+                  size="icon"
+                  className="h-12 w-12 shrink-0 rounded-full"
+                  onClick={() => setFag((prevFag) => prevFag + 1)}
+                  disabled={fag >= 50}
+                >
+                  <PlusIcon className="h-4 w-4" />
+                  <span className="sr-only">Legg til time</span>
+                </Button>
+              </div>
             </div>
           </CardContent>
         </Card>
