3569803c6bf1fa152ae149ee094d7d2ed45962bb
Ole Svennevig <69307900+osvennevig@users.noreply.github.com>
2024-05-23 08:38:22 +0200

    Delete app.tsx

--- app/routes/app.tsx
+++
@@ -1,6 +0,0 @@
-import { type LoaderFunctionArgs, redirect } from "@remix-run/node";
-//redirect to '/' for users with this url bookmarked
-
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  return redirect("/");
-};
