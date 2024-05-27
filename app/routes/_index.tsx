4e7bee6fdce1f244873f85d28a68650b1cc8b319
Ole Svennevig <69307900+osvennevig@users.noreply.github.com>
2024-05-27 13:27:17 +0200

    implement insert action with remix fetcher.Form

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,10 @@
-import type { MetaFunction } from "@remix-run/node";
+import { ActionFunctionArgs, json, type MetaFunction } from "@remix-run/node";
+import { useLoaderData, useFetcher } from "@remix-run/react";
 import Calculation from "~/components/calculation";
-import Campaign from "~/components/campaign";
+//import Campaign from "~/components/campaign";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { supabase } from "~/supabase";
 
 export const meta: MetaFunction = () => {
   return [
@@ -30,7 +34,49 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+// export const loader = async () => {
+//   const { data, error } = await supabase.from("messages").select("id");
+//   return json({ messages: data, error });
+// };
+
+// handle the insertion of a submission on the server
+export const action = async ({ request }: ActionFunctionArgs) => {
+  let formData = await request.formData();
+  let { _action, ...values } = Object.fromEntries(formData);
+
+  if (_action === "add") {
+    console.log("values:", values);
+    const email = values?.email;
+    const name = values?.name.toString();
+
+    console.log(formData);
+    console.log(values);
+
+    if (email) {
+      console.log("Missing email");
+      return new Response("Email", { status: 400 });
+    }
+
+    const { error } = await supabase.from("messages").insert([{ name }]);
+
+    if (error) {
+      console.log("Error", error.message);
+
+      return new Response(error.message, { status: 500 });
+    }
+    // Return a success response
+    console.log("Form submitted");
+    //return new Response("Form submitted successfully", { status: 200 });
+    return json({ message: `Takk for din signatur, ${name}!` });
+  }
+};
+
 export default function Index() {
+  //const { messages, error } = useLoaderData<typeof loader>();
+  const fetcher = useFetcher();
+  const isAdding = fetcher.state === "submitting";
+  const successMessage = fetcher.data?.message;
+
   return (
     <div>
       <div className="mx-4 mt-8 mb-8 text-center">
@@ -39,6 +85,70 @@ export default function Index() {
         </h1>
         <p className="text-lg">Gjelder for høsten 2023 og våren 2024</p>
       </div>
+
+      <div className="mx-auto max-w-sm mb-6">
+        <div className="mx-3">
+          <fetcher.Form method="post">
+            <div className="bg-primary/20 max-w-sm mx-auto p-6 rounded-lg border border-primary/60">
+              <p className="font-bold text-xl mb-2 leading-tight">
+                Opprop mot fraværsgrensen!
+              </p>
+
+              {/* {messages ? (
+              <div className="mx-auto max-w-xl mb-1">
+                <p className="text-sm">
+                  Antall signaturer: <b>{messages.length}</b>
+                </p>
+              </div>
+            ) : // <div className="mx-auto max-w-xl text-center">
+            //   Ingen data fra supabase
+            // </div>
+            null} */}
+
+              <span className="border-b h-[1px]"></span>
+              <p className="text-sm">
+                <b>Underskriftskampanje</b> mot fraværsgrensen. Signer med
+                navnet ditt her!
+              </p>
+              <Input
+                name="name"
+                required
+                className="bg-secondary mt-4 border-primary/60"
+                placeholder="Fornavn"
+              />
+
+              <div className="hidden">
+                <Input
+                  name="email"
+                  placeholder="e-post"
+                  autoComplete="off"
+                  tabIndex={-1}
+                  type="text"
+                />
+              </div>
+
+              <Button
+                variant="default"
+                className="w-full mt-4"
+                disabled={isAdding}
+                type="submit"
+                name={"_action"}
+                value={"add"}
+                aria-label="Send"
+              >
+                {isAdding ? "Signerer..." : "Gi din stemme!"}
+              </Button>
+
+              {successMessage ? (
+                <div className="text-center text-base mt-6 font-semibold bg-primary-foreground text-primary px-3 py-2 rounded-sm w-fit mx-auto">
+                  {successMessage}
+                </div>
+              ) : null}
+            </div>
+          </fetcher.Form>
+        </div>
+      </div>
+
       <Calculation />
       <div className="m-4 mx-auto max-w-xs">
         <p className="text-sm text-center">
